1f6eeeda359b6c02dcceaa4b59c485049b34f01b
mumarualfajar <kuhaaku@gmail.com>
2023-03-21 00:46:31 +0700

    create: user repository

--- main.go
+++ main.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"bwastartup/user"
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
-	"net/http"
 )
 
 func main() {
-	//dsn := "root:@tcp(127.0.0.1:3306)/campaign?charset=utf8mb4&parseTime=True&loc=Local"
-	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	//
-	//if err != nil {
-	//	log.Fatal(err.Error())
-	//}
-	//
-	//var users []user.User
-	//
-	//db.Find(&users)
-	//
-	//println("Total user:", len(users))
-	//
-	//for _, user := range users {
-	//	println(user.Name, user.Email)
-	//}
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	//handle error for router.Run()
-	err := router.Run()
-	if err != nil {
-		return
-	}
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/campaign_startup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -44,8 +14,17 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	_ = db
 
-	c.JSON(http.StatusOK, users)
+	//userRepository := user.NewRepository(db)
+	//user := user.User{
+	//	Name: "John",
+	//}
+	//
+	//_, err = userRepository.Save(user)
+	//if err != nil {
+	//	log.Fatal(err.Error())
+	//}
+	//
+	//log.Println("User is created")
 }
